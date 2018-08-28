9f60ed05357e1d389362ecab41834fc322f85577
Kevin Griffin <kevin@kevgriffin.com>
2018-08-28 15:36:42 -0400

    Implement basic text parser

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ProjectDover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser = new CommandParser();
+            while (true)
+            {
+                Console.Write("> ");
+                var inputString = Console.ReadLine();
+
+                switch (parser.ProcessCommandText(inputString))
+                {
+                    case Command.COMMAND_QUIT:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Thanks for playing!");
+                            Environment.Exit(0);
+                        }
+                        break;
+
+                    case Command.UNKNOWN:
+                    default:
+                        {
+                            Console.WriteLine("I'm not sure what to do");
+                        }
+                        break;
+                }
+            }
+        }
+    }
+
+    public enum Command
+    {
+        UNKNOWN = -1,
+        COMMAND_QUIT
+    }
+    public class CommandParser
+    {
+        public Command ProcessCommandText(string commandText)
+        {
+            if (commandText.Equals("QUIT", StringComparison.OrdinalIgnoreCase))
+            {
+                return Command.COMMAND_QUIT;
+            }
+
+            return Command.UNKNOWN;
+        }
+    }
+}
---
+++ _notes.md
@@ -0,0 +1,29 @@
+
+- Text parser
+  - GO NORTH / NORTH / N
+  - PICK UP THE BOOK -> PICKUP BOOK -> GET BOOK -> RETRIEVE BOOK
+  - [COMMAND] + [OBJECT]
+  - TALK TO BOB -> TALK BOB
+
+- Rooms
+  - ID
+  - Description
+  - Exits (NSEWUD)
+    - Locked
+
+- Item/Inventory
+  - Pick up an item
+  - ID
+  - Description
+
+- Event manager / Game Master
+  - SOMETHING happened -> does something else need to happen?
+  - Walk into room X while holding book, Bob yells at you to stop reading
+  - `UNLOCK DOOR` while holding key, 
+  - What important things have happened? (Flag)
+    - Turned on the stove
+
+- Save/Load
+  - dump the current game state
+  - don't care about security
+  
